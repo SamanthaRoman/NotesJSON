0a75cd3edcd40c9abbd06b656112f1dba6c6871e
Samantha Roman <samantharaeroman@gmail.com>
2025-11-17 20:46:37 -0500

    Initial Commit

---
+++ 108-NotesJSON/_08_NotesJSONApp.swift
@@ -0,0 +1,17 @@
+//
+//  _08_NotesJSONApp.swift
+//  108-NotesJSON
+//
+//  Created by Samantha Roman on 11/17/25.
+//
+
+import SwiftUI
+
+@main
+struct _08_NotesJSONApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
